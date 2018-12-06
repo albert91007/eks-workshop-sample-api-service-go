52d36e4e03d596b1a191ee10649fb2c048950bd9
albert91007 <34915462+albert91007@users.noreply.github.com>
2018-12-06 16:08:34 +0800

    Hello Cloud Day

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello AWS"}
+		res := &response{Message: "Hello Cloud Day"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
